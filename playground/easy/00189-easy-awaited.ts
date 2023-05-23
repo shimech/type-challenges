6bba26b31ee0f498243fdd91f18deadbd9f68766
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-05-23 21:57:31 +0900

    00189

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,27 +22,32 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyAwaited<T> = any
+type Thenable<T> = { then: (onfulfilled: (arg: T) => any) => any };
+
+type MyAwaited<T> = T extends Promise<infer U>
+  ? MyAwaited<U>
+  : T extends Thenable<infer V>
+  ? V
+  : T;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type X = Promise<string>
-type Y = Promise<{ field: number }>
-type Z = Promise<Promise<string | number>>
-type Z1 = Promise<Promise<Promise<string | boolean>>>
-type T = { then: (onfulfilled: (arg: number) => any) => any }
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
 
 type cases = [
   Expect<Equal<MyAwaited<X>, string>>,
   Expect<Equal<MyAwaited<Y>, { field: number }>>,
   Expect<Equal<MyAwaited<Z>, string | number>>,
   Expect<Equal<MyAwaited<Z1>, string | boolean>>,
-  Expect<Equal<MyAwaited<T>, number>>,
-]
+  Expect<Equal<MyAwaited<T>, number>>
+];
 
-// @ts-expect-error
-type error = MyAwaited<number>
+type error = MyAwaited<number>;
 
 /* _____________ Further Steps _____________ */
 /*
