aa1592fc5ca8b9e5dee22dd52fa5e624e5773004
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-05-23 21:37:40 +0900

    00014

--- playground/easy/00014-easy-first-of-array.ts
+++ playground/easy/00014-easy-first-of-array.ts
@@ -22,24 +22,24 @@
 
 /* _____________ Your Code Here _____________ */
 
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<First<[3, 2, 1]>, 3>>,
   Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
   Expect<Equal<First<[]>, never>>,
-  Expect<Equal<First<[undefined]>, undefined>>,
-]
+  Expect<Equal<First<[undefined]>, undefined>>
+];
 
 type errors = [
   // @ts-expect-error
-  First<'notArray'>,
+  First<"notArray">,
   // @ts-expect-error
-  First<{ 0: 'arrayLike' }>,
-]
+  First<{ 0: "arrayLike" }>
+];
 
 /* _____________ Further Steps _____________ */
 /*
