5b470ae1ef73bd141d24c12ab1ba512144cfbc8d
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-05-24 21:24:43 +0900

    00533

--- playground/easy/00533-easy-concat.ts
+++ playground/easy/00533-easy-concat.ts
@@ -18,23 +18,28 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Concat<T, U> = any
+type Concat<T extends readonly any[], U extends readonly any[]> = [...T, ...U];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-const tuple = [1] as const
+const tuple = [1] as const;
 
 type cases = [
   Expect<Equal<Concat<[], []>, []>>,
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
-]
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >
+];
 
 // @ts-expect-error
-type error = Concat<null, undefined>
+type error = Concat<null, undefined>;
 
 /* _____________ Further Steps _____________ */
 /*
