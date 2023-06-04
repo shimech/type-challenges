647f01fbe0db8d65ade33ab5c21c0706e8df93b7
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-06-04 20:11:22 +0900

    03057

--- playground/easy/03057-easy-push.ts
+++ playground/easy/03057-easy-push.ts
@@ -18,16 +18,16 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Push<T, U> = any
+type Push<T extends readonly unknown[], U> = [...T, U];
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
-  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
-  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
-]
+  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
