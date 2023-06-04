36b5da4416e413d37ab9ea72db83049b7c6c8846
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-06-04 20:16:07 +0900

    03312

--- playground/easy/03312-easy-parameters.ts
+++ playground/easy/03312-easy-parameters.ts
@@ -20,20 +20,24 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer S
+) => any
+  ? S
+  : never;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-const foo = (arg1: string, arg2: number): void => {}
-const bar = (arg1: boolean, arg2: { a: 'A' }): void => {}
-const baz = (): void => {}
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: "A" }): void => {};
+const baz = (): void => {};
 
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
-  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
-  Expect<Equal<MyParameters<typeof baz>, []>>,
-]
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>
+];
 
 /* _____________ Further Steps _____________ */
 /*
