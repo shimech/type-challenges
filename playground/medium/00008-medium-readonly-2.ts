d4869c6edd839a24d9192c5ac871019d405c3f6f
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-06-04 20:48:20 +0900

    00008

--- playground/medium/00008-medium-readonly-2.ts
+++ playground/medium/00008-medium-readonly-2.ts
@@ -34,37 +34,38 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> &
+  Readonly<Pick<T, K>>;
 
 /* _____________ Test Cases _____________ */
-import type { Alike, Expect } from '@type-challenges/utils'
+import type { Alike, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
-  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
-]
+  Expect<Alike<MyReadonly2<Todo1, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "title" | "description">, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, "description">, Expected>>
+];
 
 // @ts-expect-error
-type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+type error = MyReadonly2<Todo1, "title" | "invalid">;
 
 interface Todo1 {
-  title: string
-  description?: string
-  completed: boolean
+  title: string;
+  description?: string;
+  completed: boolean;
 }
 
 interface Todo2 {
-  readonly title: string
-  description?: string
-  completed: boolean
+  readonly title: string;
+  description?: string;
+  completed: boolean;
 }
 
 interface Expected {
-  readonly title: string
-  readonly description?: string
-  completed: boolean
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
 }
 
 /* _____________ Further Steps _____________ */
