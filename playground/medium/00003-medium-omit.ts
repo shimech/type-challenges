3a6baa6767298271f644ee645826de9a63457eda
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2023-06-04 20:41:12 +0900

    00003

--- playground/medium/00003-medium-omit.ts
+++ playground/medium/00003-medium-omit.ts
@@ -30,32 +30,34 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyOmit<T, K> = any
+type MyOmit<T, K extends keyof T> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
-  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
-]
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
 
 // @ts-expect-error
-type error = MyOmit<Todo, 'description' | 'invalid'>
+type error = MyOmit<Todo, "description" | "invalid">;
 
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
 interface Expected1 {
-  title: string
-  completed: boolean
+  title: string;
+  completed: boolean;
 }
 
 interface Expected2 {
-  title: string
+  title: string;
 }
 
 /* _____________ Further Steps _____________ */
